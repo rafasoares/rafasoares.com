7bd4319a7bbfc5b6a7c73834d62b4723e917b8ed
Rafael Soares <893971+rafasoares@users.noreply.github.com>
2026-04-08 15:33:53 +0200

    improve index with WIP message and contact link

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+
 useTitle('Home')
+
+const links = ref<ButtonProps[]>([
+  {
+    label: 'Get in touch',
+    icon: 'i-tabler-mail',
+    to: '/contact',
+  },
+])
 </script>
 
 <template>
   <u-page>
-    <u-page-hero title="Hi, I'm Rafael." />
+    <u-page-hero
+      title="Hi, I'm Rafael."
+      description="This website is currently being rebuilt. Please check again later."
+      :links
+    >
+      <template #headline>
+        <u-badge
+          color="success"
+          variant="soft"
+          class="rounded-full"
+        >
+          <u-chip
+            color="success"
+            standalone
+            inset
+          />
+          Available for contract and full-time work
+        </u-badge>
+      </template>
+    </u-page-hero>
   </u-page>
 </template>
