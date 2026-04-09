36240824b08fb5f50d441555bd4eb3bbdc7008fd
Rafael Soares <893971+rafasoares@users.noreply.github.com>
2026-04-09 19:45:50 +0200

    add error page

---
+++ app/error.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import type { NuxtError } from 'nuxt/app'
+
+const { error } = defineProps<{
+  error: NuxtError
+}>()
+
+useTitle(`Error ${error.status} - ${error.statusText}`)
+</script>
+
+<template>
+  <u-app>
+    <nuxt-layout>
+      <u-main>
+        <u-error
+          :error
+          :clear="{
+            color: 'neutral',
+            icon: 'i-tabler-arrow-left',
+            size: 'lg',
+          }"
+        />
+      </u-main>
+    </nuxt-layout>
+  </u-app>
+</template>
